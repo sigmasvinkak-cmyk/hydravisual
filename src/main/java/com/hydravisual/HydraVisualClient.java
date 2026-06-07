1f346d98f719717b642d057ebd6ed8d94885298b
sigmasvinkak-cmyk <sigmasvinkak@gmail.com>
2026-06-07 22:25:02 +0300

    v1.6.1: version bump for launcher update

--- src/main/java/com/hydravisual/HydraVisualClient.java
+++ src/main/java/com/hydravisual/HydraVisualClient.java
@@ -19,7 +19,7 @@ public class HydraVisualClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         INSTANCE = this;
-        LOGGER.info("🐉 {} v1.6.0 загружен!", MOD_NAME);
+        LOGGER.info("🐉 {} v1.6.1 загружен!", MOD_NAME);
 
         // Register custom sounds FIRST
         ModSounds.register();
